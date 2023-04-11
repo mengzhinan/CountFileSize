d3c26b92665a7b60f66bad18152e26a62ce21185
duke <duke@zhihu.com>
2023-04-11 19:17:21 +0800

    优化逻辑，把单位参数调整到最后

--- src/CountFileSizeWrapper.java
+++ src/CountFileSizeWrapper.java
@@ -2,19 +2,18 @@ import java.io.File;
 import java.util.Locale;
 
 public class CountFileSizeWrapper {
-    private static final String BASE_PATH = "/Users/duke/Desktop/";
-    private static final String UNIT_KB = "kb";
-    private static final String UNIT_MB = "mb";
-    private static final String UNIT_GB = "gb";
-    private static final String UNIT_EXPLAIN = "KB, MB, GB";
+    private static final String UNIT_KB = "kb".toLowerCase(Locale.ROOT);
+    private static final String UNIT_MB = "mb".toLowerCase(Locale.ROOT);
+    private static final String UNIT_GB = "gb".toLowerCase(Locale.ROOT);
+    private static final String UNIT_DEFAULT = UNIT_MB;
+    private static final long UNIT_DEFAULT_LONG = 1024 * 1024;
+
 
     public static void main(String[] args) {
         if (!checkParams(args)) {
-            System.out.println("参数说明(参数之间使用空格分割)：");
-            System.out.println("参数1：KB");
-            System.out.println("参数2：/User/xxx/a.txt");
-            System.out.println("参数3：/User/xxx/f.txt");
-            System.out.println("参数N：。。。");
+            System.out.println("格式说明：java -jar xxx/CountFileSize.jar xxx/a.txt xxx/b.app xxx/z.pdf [单位: KB/MB/GB]");
+            System.out.println("例如：计算文件 a.txt, k.pdf 的大小，单位为 MB");
+            System.out.println("Code>：java -jar xxx/CountFileSize.jar a.txt k.pdf MB");
             return;
         }
         String result = countFile(args);
@@ -23,41 +22,43 @@ public class CountFileSizeWrapper {
 
     private static boolean checkParams(String[] args) {
         if (args == null || args.length == 0) {
-            System.out.println("请输入参数，格式化单位 和 文件路径");
-            return false;
-        }
-        if (args.length < 2) {
-            System.out.println("请输入文件路径");
-            return false;
-        }
-        if (!args[0].toLowerCase(Locale.ROOT).equals(UNIT_KB)
-                && !args[0].toLowerCase(Locale.ROOT).equals(UNIT_MB)
-                && !args[0].toLowerCase(Locale.ROOT).equals(UNIT_GB)) {
-            System.out.println("格式化单位必须是 " + UNIT_EXPLAIN + " 之一");
+            System.out.println("请输入有效的参数！");
             return false;
         }
         return true;
     }
 
     private static String countFile(String[] args) {
-        String unitStr = args[0].toLowerCase(Locale.ROOT);
-        long unitLong = 1024;
-        if (UNIT_MB.toLowerCase(Locale.ROOT).equals(unitStr)) {
+        // args != null && args.length > 0
+        String lastItem = args[args.length - 1].toLowerCase(Locale.ROOT);
+        String unitStr = UNIT_DEFAULT;
+        long unitLong = UNIT_DEFAULT_LONG;
+        boolean hasUnit = false;
+        if (UNIT_KB.equals(lastItem)) {
+            unitStr = UNIT_KB;
+            unitLong = 1024;
+            hasUnit = true;
+        } else if (UNIT_MB.equals(lastItem)) {
+            unitStr = UNIT_MB;
             unitLong = 1024 * 1024;
-        } else if (UNIT_GB.toLowerCase(Locale.ROOT).equals(unitStr)) {
+            hasUnit = true;
+        } else if (UNIT_GB.equals(unitStr)) {
+            unitStr = UNIT_GB;
             unitLong = 1024 * 1024 * 1024;
+            hasUnit = true;
         }
         StringBuilder sb = new StringBuilder(">>> 输出计算结果: \r\n");
-        // 第一个是单位参数，跳过
-        for (int i = 1; i < args.length; i++) {
+        int lastIndex = args.length;
+        if (hasUnit) {
+            // 最后一个是单位参数，跳过
+            lastIndex -= 1;
+        }
+        for (int i = 0; i < lastIndex; i++) {
             String item = args[i];
             File file = new File(item);
             if (!file.exists()) {
-                file = new File(BASE_PATH, item);
-            }
-            if (!file.exists()) {
                 sb.append(file.getName());
-                sb.append(" 不存在");
+                sb.append(" 文件不存在");
                 sb.append("\r\n");
             } else {
                 sb.append(file.getName());
