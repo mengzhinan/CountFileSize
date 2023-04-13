0ffb9d5d4c3ab064de98e92c41be83fb1831a7fb
duke <duke@zhihu.com>
2023-04-13 19:56:17 +0800

    ui

---
+++ src/ui/MyPage.java
@@ -0,0 +1,35 @@
+package ui;
+
+import javax.swing.*;
+
+public class MyPage {
+    private static final String PAGE_TITLE = "CountFileSize";
+
+    public static void main(String[] args) {
+        // 显示应用 GUI
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                showUI();
+            }
+        });
+    }
+
+    private static void showUI(){
+        // 确保一个漂亮的外观风格
+        JFrame.setDefaultLookAndFeelDecorated(true);
+
+        // 创建及设置窗口
+        JFrame frame = new JFrame(PAGE_TITLE);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // 添加 "Hello World" 标签
+        JLabel label = new JLabel("Hello World");
+        frame.getContentPane().add(label);
+
+        // 显示窗口
+        frame.pack();
+        frame.setVisible(true);
+
+    }
+
+}
